e0eadccdc1987f2bf1e64e57a4e815d7761b79bd
Vikas Saini <vspassion007@gmail.com>
2021-03-20 10:24:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var NSEAPI = API.NSE;
 
 var app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port " + process.env.PORT || 3000);
 });
 
 // National Stock Exchange (NSE) APIS
